e5801cbe8767eb3a8edf28cef3deab4049a56f20
A_ER_Y <erayozcan57@gmail.com>
2020-11-04 19:48:04 +0100

    made exercise 2 of chapter 6

--- src/be/pxl/ja/oefening2/GameBrowser.java
+++ src/be/pxl/ja/oefening2/GameBrowser.java
@@ -1,25 +1,31 @@
 package be.pxl.ja.oefening2;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class GameBrowser {
-
 	private GameCollection gameCollection;
-
 
 	public GameBrowser(GameCollection gameCollection) {
 		this.gameCollection = gameCollection;
 	}
 
 	public List<VideoGame> showFreeGames() {
-		throw new UnsupportedOperationException("Implement this method");
+        return gameCollection.selectGames((videoGame) -> videoGame.getPrice() == 0);
 	}
 
 	public List<VideoGame> showGamesInGenre(String action) {
-		throw new UnsupportedOperationException("Implement this method");
+        return gameCollection.selectGames((videoGame) -> videoGame.getGenres().toString().toLowerCase().contains(action.toLowerCase()));
 	}
 
 	public List<VideoGame> showGamesForSearch(String cd) {
-		throw new UnsupportedOperationException("Implement this method");
+        Predicate<VideoGame> filter = new Predicate<>() {
+            @Override
+            public boolean test(VideoGame videoGame) {
+                return videoGame.getName().toLowerCase().contains(cd.toLowerCase());
+            }
+        };
+
+        return gameCollection.selectGames(filter);
 	}
 }
--- src/be/pxl/ja/oefening2/GameCollection.java
+++ src/be/pxl/ja/oefening2/GameCollection.java
@@ -2,12 +2,24 @@ package be.pxl.ja.oefening2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class GameCollection {
 	private List<VideoGame> videoGames = new ArrayList<>();
 
 	public void addGame(VideoGame game) {
-		throw new UnsupportedOperationException("Implement this method");
+		videoGames.add(game);
 	}
 
+    public List<VideoGame> selectGames(Predicate<VideoGame> filter) {
+        List<VideoGame> filteredVideoGames = new ArrayList<>();
+
+        for (VideoGame videoGame : videoGames) {
+            if (filter.test(videoGame)) {
+                filteredVideoGames.add(videoGame);
+            }
+        }
+
+        return filteredVideoGames;
+    }
 }
